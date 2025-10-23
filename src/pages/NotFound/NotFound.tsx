a49214dc2c939c2396b7405a63c62e7eb7d859b7
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2025-10-23 22:55:23 +0300

    feat: добавляет контент страницы 404

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 import { useTypedDispatch } from "../../app/redux/hooks/redux";
 import { selectArea } from "../../app/redux/reducers/vacanciesSlice";
+import NotFoundImg from "../../app/assets/not-found-page/not-found.png";
 
 import styles from "./NotFound.module.css";
 
@@ -10,13 +11,32 @@ export const NotFound = () => {
 
   return (
     <div className={styles["not-found__container"]}>
-      Bad GateWay
-      <Link
-        to={"/vacancies/moscow"}
-        onClick={() => dispatch(selectArea("Москва"))}
-      >
-        На главную
-      </Link>
+      <div className={styles["not-found__data"]}>
+        <div>
+          <h1 className={styles["not-found__title"]}>
+            Упс! Такой страницы не существует
+          </h1>
+          <p className={styles["not-found__to-begin"]}>
+            Давайте перейдём к началу.
+          </p>
+        </div>
+
+        <Link
+          className={styles["not-found__button"]}
+          to={"/vacancies/moscow"}
+          onClick={() => dispatch(selectArea("Москва"))}
+        >
+          На главную
+        </Link>
+      </div>
+
+      <img
+        className={styles["not-found__img"]}
+        src={NotFoundImg}
+        alt="not-found-image"
+        width="640"
+        height="336"
+      />
     </div>
   );
 };
